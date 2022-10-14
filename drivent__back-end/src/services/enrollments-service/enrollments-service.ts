4f9379b36f0b7ab853e78302098bea2f3ead8220
vcsil <vcsilva@poli.ufrj.br>
2022-10-14 17:55:00 -0300

    fix: resolve bugs de criação de endereço

--- drivent__back-end/src/services/enrollments-service/enrollments-service.ts
+++ drivent__back-end/src/services/enrollments-service/enrollments-service.ts
@@ -35,34 +35,44 @@ export async function getOneWithAddressByUserId(userId: number) {
 }
 
 export async function createOrUpdateEnrollmentWithAddress(params: CreateEnrollmentParams) {
-  const createOrUpdateParams = {
+  const createOrUpdateEnrollmentParams = {
     name: params.name,
     cpf: params.cpf,
     birthday: params.birthday,
     phone: params.phone,
     userId: params.userId,
-    Address: {
-      create: {
-        cep: params.address.cep,
-        street: params.address.street,
-        city: params.address.city,
-        number: params.address.number,
-        state: params.address.state,
-        neighborhood: params.address.neighborhood,
-        ...(params.address.addressDetail && { addressDetail: params.address.addressDetail }),
-      },
-    },
-  };
+  }
 
-  await prisma.enrollment.upsert({
+  const enrollment = await prisma.enrollment.upsert({
     where: {
       userId: params.userId,
     },
-    create: createOrUpdateParams,
-    update: createOrUpdateParams,
+    create: createOrUpdateEnrollmentParams,
+    update: createOrUpdateEnrollmentParams,
     include: {
       Address: true,
     },
+  });
+
+  const createOrUptadeAddressParams = {
+    cep: params.address.cep,
+    street: params.address.street,
+    city: params.address.city,
+    number: params.address.number,
+    state: params.address.state,
+    neighborhood: params.address.neighborhood,
+    ...(params.address.addressDetail && { addressDetail: params.address.addressDetail }),
+  };
+
+  await prisma.address.upsert({
+    where: {
+      enrollmentId: enrollment.id,
+    },
+    create: {
+      ...createOrUptadeAddressParams,
+      enrollmentId: enrollment.id
+    },
+    update: createOrUptadeAddressParams,
   });
 }
 
